1cfdfedb219114635077898e669b186ea836f482
hexagon8899 <63066753+hexagon8899@users.noreply.github.com>
2022-07-01 20:31:57 +0100

    cleaned up code and added labels

--- example.py
+++ example.py
@@ -5,5 +5,12 @@ def randomnum():
     value = random.random()
     return value
 
-g=Grapher(randomnum, hackermode=True)
+g=Grapher(
+    randomnum,
+    hackermode=True,
+    save=True,
+    displaymessage = {
+    60: 'this is a label',
+    61: 'this is another label'
+    })
 g.start()
--- grapher.py
+++ grapher.py
@@ -4,37 +4,15 @@ import tkinter.scrolledtext as st
 from ctypes import windll
 windll.shcore.SetProcessDpiAwareness(1)
 
-def line(length, width, label):
-    line =  '░'*length
-    line += ' '*(5+width-len(line))
-    line += label
-    line += ' '*(30+width-len(line))
-    line += 'a'
-    line += '\n'
-    return line
 
-def all_lines(l, width):
-    s = ''
-    l = [i if type(i) == list else [i, str(i)] for i in l]
-    
-    data = [i[0] for i in l]
-    low = min(data)
-    high = max(data)
-    i = 0
-    for elem in l:
-        i += 1
-        
-        try: length = int(width*(elem[0]-low)/(high-low))
-        except ZeroDivisionError: length = width//2
-        
-        s += line(length, width, elem[1])
-    s = s[:-1]
-    return s
+
+
+
 
 class Grapher(Tk):
-    def __init__(self, func, interval=200, width=40, height=100, save = False, hackermode = False):
+    def __init__(self, func, interval=200, width=40, height=100, displaymessage = {}, save = False, hackermode = False):
         '''
-func: the function to get the data from
+func: the function to get the data from, should return float
 interval: delay between function calls (ms).
 width: width of the graph
 height: how many lines of the graph are shown
@@ -50,8 +28,9 @@ hackermode: makes you look like a movie hacker'''
         self.frame = ttk.Frame(self)
         self.frame.pack()
         
-        self.l = [[0, 'unrecorded'] for i in range(height)]
+        self.l = [0 for i in range(height)]
         self.save = save
+        self.displaymessage = displaymessage
         self.func, self.interval, self.width, self.height = func, interval, width, height
         #to save memory, dont save all the recorded data when not saving to file 
         if save:
@@ -62,34 +41,75 @@ hackermode: makes you look like a movie hacker'''
         self.graphed = st.ScrolledText(self.frame, width=189, height=50)
         self.graphed.configure(bg='black', foreground=('lightgreen' if hackermode else 'white'))
         self.graphed.grid(row=0)
-        self.graphed.insert(1.0,all_lines(self.l, self.width))
+        self.graphed.insert(1.0,self.all_lines(self.l))
         self.graphed.see(END)
         self.graph()
         self.attributes('-fullscreen', True)
     
     #the grapher itself
     def graph(self): 
-        l = self.l
             
         x = self.func()
         if self.save:
             self.saveData.append(x)
-        l.append(x)
-        if len(l) > self.height:
-            del l[0:len(l)-self.height-1]
+        self.l.append(x)
+        if len(self.l) > self.height:
+            del self.l[0:len(self.l)-self.height-1]
         
-        s = all_lines(l, self.width)
+        s = self.all_lines(self.l)
         
         self.graphed.replace(1.0, END, s)
-        self.l = l
         self.after(self.interval, self.graph)
     
     #save data to a file
     def savetofile(self):
         f = open('data.txt', 'w+', encoding='utf-8')
-        s = all_lines(self.saveData, self.width)
+        s = self.all_lines(self.saveData)
         f.write(s)
 
     def start(self):
         self.mainloop()
+    
+    def all_lines(self, l):
+        '''
+this is the thing that generates all the text.
+
+l: a list including the input date
+width: width of the graph'''
+        s = ''
+        
+        low = min(l)
+        high = max(l)
+        keys = self.displaymessage.keys()
+        i = 0
+        for elem in l:
+            i += 1 
+            if i in keys:
+                message = self.displaymessage[i]
+            else:
+                message = ''
+            s += self.line(elem, low, high, message)
+        s = s[:-1]
+        return s
+    
+    def line(self, data: float, low:float, high:float, label: str):
+        '''
+generates a single line for the graph
+
+data: the length of the bar
+low: lower bound of the bar
+high: upper bound of the bar
+label: the text to put directly after the bar. e '''
+    
+        try: length = int(self.width*(data-low)/(high-low))
+        except ZeroDivisionError: length = self.width//2
+        
+        
+        line =  '░'*length
+        line += ' '*(5+self.width-len(line))
+        line += str(data)
+        line += ' '*(30+self.width-len(line))
+        line += label
+        line += '\n'
+        return line
 
